8f1351f006a4a942080dbee7035215f0df02eaac
janeffer945 <janeffernjeri945@gmail.com>
2022-06-23 15:41:57 +0300

    register form

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Profile, Post,NeighbourHood,Business
+
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Business)
+
+
+admin.site.register(NeighbourHood)
